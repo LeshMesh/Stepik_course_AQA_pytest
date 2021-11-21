7af233b59b129681d3b99c82330ed61ae696ae2d
LeshMesh <ivlex37rus@gmail.com>
2021-11-21 16:24:01 +0300

    Added code

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button_add_basket(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     time.sleep(2)
     button = browser.find_elements_by_css_selector('button.btn')
